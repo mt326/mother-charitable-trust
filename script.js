46c31c1a27095873bbeaba91d9056257162918a3
mt326 <mothertrust326@gmail.com>
2025-03-24 20:33:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add interactive JavaScript functionality here if needed.
+// For example, a simple alert when the page loads:
+window.onload = function() {
+    alert("Welcome to Mother Charitable Series Trust Group!");
+};
